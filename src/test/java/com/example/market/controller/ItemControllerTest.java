6fe7d9b685b6ff1150656c6d79eb897f11a06a94
YunByungil <dbsquddlfz@naver.com>
2023-06-29 17:07:15 +0900

    :white_check_mark: 아이템 단일 조회 API 테스트 완료

--- src/test/java/com/example/market/controller/ItemControllerTest.java
+++ src/test/java/com/example/market/controller/ItemControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.market.controller;
 
+import com.example.market.domain.entity.Item;
 import com.example.market.dto.ItemCreateRequestDto;
+import com.example.market.repository.ItemRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,10 +21,13 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith({RestDocumentationExtension.class})
@@ -33,8 +39,13 @@ class ItemControllerTest {
 
     private MockMvc mvc;
 
+    @Autowired
+    ItemRepository itemRepository;
+
     @BeforeEach
     public void setUp(RestDocumentationContextProvider restDocumentation) {
+        itemRepository.deleteAll();
+
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(documentationConfiguration(restDocumentation)
@@ -42,6 +53,11 @@ class ItemControllerTest {
                 .withRequestDefaults(prettyPrint())
                 .withResponseDefaults(prettyPrint()))
                 .build();
+    }
+
+    @AfterEach
+    void end() {
+        itemRepository.deleteAll();
     }
 
     private ItemCreateRequestDto createRequestDto() {
@@ -82,6 +98,37 @@ class ItemControllerTest {
                 ));
 
         // then
+    }
+
+    @DisplayName("아이템 단일 조회 API")
+    @Test
+    void readItemOne() throws Exception {
+        // given
+        String url = "https://localhost:8080/items/";
+
+        ItemCreateRequestDto dto = createRequestDto();
+        Item savedItem = itemRepository.save(dto.toEntity());
+
+        // when
+        mvc.perform(get(url + "{id}", savedItem.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(dto.getTitle()))
+                .andExpect(jsonPath("$.description").value(dto.getDescription()))
+                .andDo(document("/items-get-one",
+                        responseFields(
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("description").description("내용"),
+                                fieldWithPath("minPriceWanted").description("가격"),
+                                fieldWithPath("status").description("판매여부")
+                        ),
+                        pathParameters(
+                                parameterWithName("id").description("Item 번호")
+                        )));
+
+        // then
 
     }
+
+
 }
