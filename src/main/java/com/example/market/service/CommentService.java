a4ada9bc8a6e72bd5936f3c7fd4601b334e0697b
YunByungil <dbsquddlfz@naver.com>
2023-07-02 01:39:59 +0900

    :sparkles: 댓글 등록시 존재하지 않는 아이템 품목일 경우 예외 처리 기능 구현

--- src/main/java/com/example/market/service/CommentService.java
+++ src/main/java/com/example/market/service/CommentService.java
@@ -1,11 +1,15 @@
 package com.example.market.service;
 
+import com.example.market.domain.entity.Item;
 import com.example.market.dto.comment.request.CommentCreateRequestDto;
 import com.example.market.repository.CommentRepository;
+import com.example.market.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,9 +18,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final ItemRepository itemRepository;
 
     @Transactional
     public void create(Long itemId, CommentCreateRequestDto dto) {
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         commentRepository.save(dto.toEntity(itemId));
     }
 }
