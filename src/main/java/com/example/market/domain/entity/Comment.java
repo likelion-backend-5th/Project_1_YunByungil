3e47a1b54e0eab732215007a4e389f4b091ab0fa
YunByungil <dbsquddlfz@naver.com>
2023-07-02 01:00:03 +0900

    :sparkles: Comment Entity 생성

---
+++ src/main/java/com/example/market/domain/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.example.market.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long item_id;
+    private String writer;
+    private String password;
+    private String content;
+    private String reply;
+}
