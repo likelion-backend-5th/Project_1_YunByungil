f511b7f09b2749b1454068fa931abb71d1cd5d07
YunByungil <dbsquddlfz@naver.com>
2023-07-02 01:40:38 +0900

    :white_check_mark: 존재하지 않는 아이템에 댓글 등록시 예외 발생 테스트 완료

--- src/test/java/com/example/market/service/CommentServiceTest.java
+++ src/test/java/com/example/market/service/CommentServiceTest.java
@@ -1,13 +1,17 @@
 package com.example.market.service;
 
 import com.example.market.domain.entity.Comment;
+import com.example.market.domain.entity.Item;
 import com.example.market.dto.comment.request.CommentCreateRequestDto;
 import com.example.market.repository.CommentRepository;
+import com.example.market.repository.ItemRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,11 +27,20 @@ class CommentServiceTest {
     @Autowired
     CommentRepository commentRepository;
 
+    @Autowired
+    ItemRepository itemRepository;
+
+    @AfterEach
+    void end() {
+        itemRepository.deleteAll();
+        commentRepository.deleteAll();
+    }
+
     @DisplayName("댓글 생성 기능 테스트")
     @Test
     void createComment() {
         // given
-        Long itemId = 3L;
+        Long itemId = createItem();
 
         CommentCreateRequestDto dto = CommentCreateRequestDto.builder()
                 .writer("작성자")
@@ -46,6 +59,38 @@ class CommentServiceTest {
         assertThat(comment.getPassword()).isEqualTo(dto.getPassword());
         assertThat(comment.getContent()).isEqualTo(dto.getContent());
         assertThat(comment.getItem_id()).isEqualTo(itemId);
+    }
+    
+    @DisplayName("존재하지 않는 물품에 댓글 작성시 예외 발생")
+    @Test
+    void notExistItemException() {
+        // given
+        Long itemId = 3L;
 
+        CommentCreateRequestDto dto = CommentCreateRequestDto.builder()
+                .writer("작성자")
+                .password("비밀번호")
+                .content("내용")
+                .build();
+
+        // when
+        assertThatThrownBy(() -> {
+            commentService.create(itemId, dto);
+        }).isInstanceOf(ResponseStatusException.class);
+        
+        // then
+        
+    }
+
+    private Long createItem() {
+        Item item = itemRepository.save(Item.builder()
+                .title("제목")
+                .minPriceWanted(10_000)
+                .description("내용")
+                .writer("작성자")
+                .password("비밀번호")
+                .build());
+
+        return item.getId();
     }
 }
