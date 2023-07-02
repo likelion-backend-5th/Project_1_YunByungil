7a56a52682bb013fb0e5e40add4680f25963c844
YunByungil <dbsquddlfz@naver.com>
2023-07-02 15:34:20 +0900

    :white_check_mark: 댓글 수정 API 테스트 완료

--- src/test/java/com/example/market/controller/CommentControllerTest.java
+++ src/test/java/com/example/market/controller/CommentControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.market.controller;
 
+import com.example.market.domain.entity.Comment;
 import com.example.market.domain.entity.Item;
 import com.example.market.dto.comment.request.CommentCreateRequestDto;
+import com.example.market.dto.comment.request.CommentUpdateRequestDto;
+import com.example.market.repository.CommentRepository;
 import com.example.market.repository.ItemRepository;
 import com.example.market.service.CommentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
@@ -40,6 +44,9 @@ class CommentControllerTest {
 
     @Autowired
     CommentService commentService;
+
+    @Autowired
+    CommentRepository commentRepository;
 
     @Autowired
     ItemRepository itemRepository;
@@ -87,6 +94,53 @@ class CommentControllerTest {
 
         // then
 
+    }
+
+    @DisplayName("댓글 수정 API 테스트")
+    @Test
+    void updateComment() throws Exception {
+        // given
+        Long itemId = createItem();
+        Long commentId = createComment(itemId);
+
+        CommentUpdateRequestDto updateDto = CommentUpdateRequestDto.builder()
+                .writer("작성자")
+                .password("비밀번호")
+                .content("수정내용")
+                .build();
+
+        String url = "http://localhost:8080/items/{itemId}/comments/{commentId}";
+
+        // when
+        mvc.perform(put(url, itemId, commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(updateDto)))
+                .andExpect(status().isOk())
+                .andDo(document("/comments-update",
+                        requestFields(
+                                fieldWithPath("writer").description("댓글 작성자"),
+                                fieldWithPath("password").description("댓글 작성자 비밀번호"),
+                                fieldWithPath("content").description("수정 내용")
+                        ),
+                        pathParameters(
+                                parameterWithName("itemId").description("아이템 ID"),
+                                parameterWithName("commentId").description("댓글 ID")
+                        )));
+
+        // then
+
+    }
+
+    private Long createComment(Long itemId) {
+        CommentCreateRequestDto dto = CommentCreateRequestDto.builder()
+                .writer("작성자")
+                .password("비밀번호")
+                .content("내용")
+                .build();
+
+        Comment comment = commentRepository.save(dto.toEntity(itemId));
+
+        return comment.getId();
     }
 
     private Long createItem() {
