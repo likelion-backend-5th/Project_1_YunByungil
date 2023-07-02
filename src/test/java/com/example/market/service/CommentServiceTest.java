569359e22cdd8958f3aee0c98cda18cfdce561b6
YunByungil <dbsquddlfz@naver.com>
2023-07-02 11:49:07 +0900

    :white_check_mark: 댓글 조회(페이징) 기능 테스트 완료

--- src/test/java/com/example/market/service/CommentServiceTest.java
+++ src/test/java/com/example/market/service/CommentServiceTest.java
@@ -3,6 +3,7 @@ package com.example.market.service;
 import com.example.market.domain.entity.Comment;
 import com.example.market.domain.entity.Item;
 import com.example.market.dto.comment.request.CommentCreateRequestDto;
+import com.example.market.dto.comment.response.CommentListResponseDto;
 import com.example.market.repository.CommentRepository;
 import com.example.market.repository.ItemRepository;
 import org.assertj.core.api.Assertions;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -80,6 +82,44 @@ class CommentServiceTest {
         
         // then
         
+    }
+
+    @DisplayName("댓글 조회 메서드 테스트")
+    @Test
+    void readCommentList() {
+        // given
+        Long itemId = createItem();
+
+        CommentCreateRequestDto dto = CommentCreateRequestDto.builder()
+                .writer("작성자")
+                .password("비밀번호")
+                .content("내용")
+                .build();
+
+        for (int i = 0; i < 6; i++) {
+            commentService.create(itemId, dto);
+        }
+
+        // when
+        Page<CommentListResponseDto> commentListResponseDto = commentService.readCommentList(itemId, 0, 5);
+
+        // then
+        assertThat(commentListResponseDto.getTotalElements()).isEqualTo(6L);
+        assertThat(commentListResponseDto.getSize()).isEqualTo(5);
+
+    }
+
+    @DisplayName("댓글 조회시, 존재하지 않는 경우 테스트")
+    @Test
+    void notExistComment() {
+        // given
+
+        // when
+        Page<CommentListResponseDto> commentListResponseDto = commentService.readCommentList(5000L, 0, 5);
+
+        // then
+        assertThat(commentListResponseDto.getTotalElements()).isEqualTo(0L);
+
     }
 
     private Long createItem() {
