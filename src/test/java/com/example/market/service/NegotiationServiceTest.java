b44163c8ca89a071dad04ea8509e408ab5dbdc11
YunByungil <dbsquddlfz@naver.com>
2023-07-03 18:45:43 +0900

    :white_check_mark: 등록된 제안 단순 확인 조회 테스트 완료

--- src/test/java/com/example/market/service/NegotiationServiceTest.java
+++ src/test/java/com/example/market/service/NegotiationServiceTest.java
@@ -4,14 +4,19 @@ import com.example.market.domain.entity.Comment;
 import com.example.market.domain.entity.Item;
 import com.example.market.domain.entity.Negotiation;
 import com.example.market.dto.negotiation.request.NegotiationCreateRequestDto;
+import com.example.market.dto.negotiation.request.NegotiationListRequestDto;
+import com.example.market.dto.negotiation.response.NegotiationListResponseDto;
 import com.example.market.repository.CommentRepository;
 import com.example.market.repository.ItemRepository;
 import com.example.market.repository.NegotiationRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,6 +32,11 @@ class NegotiationServiceTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @AfterEach
+    void end() {
+        negotiationRepository.deleteAll();
+    }
 
     @DisplayName("제안 등록 기능 테스트")
     @Test
@@ -52,6 +62,30 @@ class NegotiationServiceTest {
 
     }
 
+    @DisplayName("등록된 제안 단순 확인 조회 메서드 테스트")
+    @Test
+    void readAllProposals() {
+        // given
+        Item item = createItem();
+        createSixNegotiation(item.getId());
+
+        int page = 0;
+        int limit = 5;
+
+        NegotiationListRequestDto listDto = NegotiationListRequestDto.builder()
+                .writer("작성자")
+                .password("비밀번호")
+                .build();
+        // when
+        Page<NegotiationListResponseDto> negotiationListResponseDto = negotiationService.readAllNegotiation(item.getId(), page, limit, listDto);
+
+        // then
+        assertThat(negotiationListResponseDto.getSize()).isEqualTo(5);
+        assertThat(negotiationListResponseDto.getTotalElements()).isEqualTo(6);
+        assertThat(negotiationListResponseDto.hasNext()).isTrue();
+
+    }
+
     private Item createItem() {
         return itemRepository.save(Item.builder()
                 .title("제목")
@@ -62,5 +96,15 @@ class NegotiationServiceTest {
                 .build());
     }
 
+    private void createSixNegotiation(Long itemId) {
+        NegotiationCreateRequestDto createDto = NegotiationCreateRequestDto.builder()
+                .writer("작성자")
+                .password("비밀번호")
+                .suggestedPrice(5_000)
+                .build();
 
+        for (int i = 0; i < 6; i++) {
+            negotiationService.createNegotiation(itemId, createDto);
+        }
+    }
 }
