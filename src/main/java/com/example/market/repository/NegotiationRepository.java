92df6dd716b36d0263c9baa7e27e9787030ef7c8
YunByungil <dbsquddlfz@naver.com>
2023-07-04 16:52:59 +0900

    :sparkles: updateNegotiationStatus() 메서드 추가
    - 구매가 확정되었을 때, 그 제안을 제외한 나머지 제안 거절로 변경하기 위한 메서드

--- src/main/java/com/example/market/repository/NegotiationRepository.java
+++ src/main/java/com/example/market/repository/NegotiationRepository.java
@@ -4,6 +4,9 @@ import com.example.market.domain.entity.Negotiation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +18,8 @@ public interface NegotiationRepository extends JpaRepository<Negotiation, Long> 
     Page<Negotiation> findAllByItemId(Long itemId, Pageable pageable);
 
     Page<Negotiation> findAllByItemIdAndWriterAndPassword(Long itemId, String writer, String password, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Negotiation n set n.status = '거절' where n.id <> :negotiationId and n.itemId = :itemId")
+    int updateNegotiationStatus(@Param("negotiationId") Long negotiationId, @Param("itemId") Long itemId);
 }
