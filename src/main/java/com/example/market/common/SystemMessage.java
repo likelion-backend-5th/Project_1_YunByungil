7e9cfa6cd122adf07eb64792a7ced4c1592296cf
YunByungil <dbsquddlfz@naver.com>
2023-07-17 19:41:29 +0900

    :recycle: SystemMessage 추가

--- src/main/java/com/example/market/common/SystemMessage.java
+++ src/main/java/com/example/market/common/SystemMessage.java
@@ -4,4 +4,15 @@ public class SystemMessage {
     public static final String CHANGE_SUGGEST_STATUS = "제안의 상태가 변경되었습니다.";
     public static final String CHANGE_SUGGEST = "제안이 수정되었습니다.";
     public static final String PURCHASE_CONFIRM = "구매가 확정되었습니다.";
+    public static final String REGISTER_COMPLETE = "등록이 완료되었습니다.";
+    public static final String UPDATE_ITEM = "물품이 수정되었습니다.";
+    public static final String DELETE_ITEM = "물품을 삭제했습니다.";
+    public static final String REGISTER_IMAGE = "이미지가 등록되었습니다.";
+    public static final String REGISTER_COMMENT = "댓글이 등록되었습니다.";
+    public static final String UPDATE_COMMENT = "댓글이 수정되었습니다.";
+    public static final String DELETE_COMMENT = "댓글이 삭제되었습니다.";
+    public static final String REGISTER_REPLY = "댓글에 답변이 추가되었습니다.";
+    public static final String REGISTER_NEGOTIATION = "구매 제안이 등록되었습니다.";
+    public static final String DELETE_REGISTER = "제안을 삭제했습니다.";
+
 }
