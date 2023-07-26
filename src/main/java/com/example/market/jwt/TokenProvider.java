80c6012f65c4fe3bad2fa9b5e22f15abb3ddf965
YunByungil <dbsquddlfz@naver.com>
2023-07-26 15:11:32 +0900

    :sparkles: TokenProvider 구현

---
+++ src/main/java/com/example/market/jwt/TokenProvider.java
@@ -0,0 +1,69 @@
+package com.example.market.jwt;
+
+import com.example.market.domain.entity.user.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@Slf4j
+@Component
+public class TokenProvider {
+
+    private Key key;
+    private final JwtParser jwtParser;
+
+    public TokenProvider(@Value("${jwt.secret}") String key) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
+        this.jwtParser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+    }
+
+    public String createAccessToken(User user) {
+        return Jwts.builder()
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plusSeconds(3600)))
+                .claim("id", user.getId())
+                .claim("role", user.getRole())
+                .signWith(key)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities =
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_" + claims.get("role")));
+
+        return new UsernamePasswordAuthenticationToken(claims.get("id"), null, authorities);
+    }
+
+    public boolean validToken(String token) {
+        try {
+            jwtParser.parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("invalidToken", e);
+            return false;
+        } catch (Exception e) {
+            log.error("invalidToken", e);
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return jwtParser.parseClaimsJws(token)
+                .getBody();
+    }
+}
