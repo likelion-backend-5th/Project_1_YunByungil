bc664e334b06ff5a4ee6128098d1f67a14e4c31d
YunByungil <dbsquddlfz@naver.com>
2023-07-27 13:43:00 +0900

    :recycle: ItemEntity, NegotiationEntity 1:N 매핑

--- src/main/java/com/example/market/domain/entity/Item.java
+++ src/main/java/com/example/market/domain/entity/Item.java
@@ -35,6 +35,9 @@ public class Item {
     @OneToMany(mappedBy = "item")
     private List<Comment> comments = new ArrayList<>();
 
+    @OneToMany(mappedBy = "item")
+    private List<Negotiation> negotiations = new ArrayList<>();
+
     @Builder
     public Item(String title, String description, String imageUrl, int minPriceWanted, User user) {
         this.title = title;
