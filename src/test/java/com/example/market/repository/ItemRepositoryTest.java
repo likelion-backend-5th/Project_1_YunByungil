214149b2d8923c732ee5bfb8d592eb877b417bcc
YunByungil <dbsquddlfz@naver.com>
2023-07-27 15:37:59 +0900

    :recycle: ItemRepository 연관관계 매핑으로 관련 로직 수정

--- src/test/java/com/example/market/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/market/repository/ItemRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.example.market.repository;
 
 import com.example.market.domain.entity.Item;
+import com.example.market.domain.entity.enums.Role;
+import com.example.market.domain.entity.user.User;
 import com.example.market.dto.item.request.ItemCreateRequestDto;
+import com.example.market.repository.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,21 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        itemRepository.deleteAll();
+        user = userRepository.save(User.builder()
+                .username("아이디")
+                .password("비밀번호")
+                .role(Role.USER)
+                .build());
+    }
+
     @DisplayName("itemRepository.save() 기능 테스트")
     @Test
     void saveItem() {
@@ -23,12 +42,10 @@ class ItemRepositoryTest {
                 .title("제목1")
                 .description("내용1")
                 .minPriceWanted(10_000)
-                .writer("작성자1")
-                .password("비밀번호1")
                 .build();
 
         // when
-        Item item = itemRepository.save(dto.toEntity());
+        Item item = itemRepository.save(dto.toEntity(user));
 
         // then
         assertThat(item.getTitle()).isEqualTo(dto.getTitle());
