64aeb16bcc33bbf969e798b7174c86dbe7072410
YunByungil <dbsquddlfz@naver.com>
2023-08-01 13:10:01 +0900

    :sparkles: SecurityConfig /js/** 경로 permitAll 추가

--- src/main/java/com/example/market/config/SecurityConfig.java
+++ src/main/java/com/example/market/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,11 +17,23 @@ public class SecurityConfig {
     private final MyCustomDsl myCustomDsl;
 
     @Bean
+    public WebSecurityCustomizer configure() {
+        return (web -> web.ignoring()
+                .requestMatchers("/static/**"));
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable);
+
         http
                 .authorizeHttpRequests(authHttp -> authHttp
                         .requestMatchers("/join", "/views/**", "/token/**")
+                        .permitAll()
+                        .requestMatchers("/", "/css/**", "/js/**")
                         .permitAll()
                         .requestMatchers(HttpMethod.GET, "/items/**")
                         .permitAll()
