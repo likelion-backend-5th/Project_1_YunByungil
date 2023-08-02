09ef2ccb072d4ad171d75dafe815f71a96e6946b
YunByungil <dbsquddlfz@naver.com>
2023-08-02 15:45:41 +0900

    :sparkles: header 토큰 검증을 하기 위한 작업 로직 추가
    - html: header 값을 추가함
    - Handler: 채팅방 연결 전 헤더에 담긴 토큰 검증 로직

---
+++ src/main/java/com/example/market/config/WebSocketStompConfig.java
@@ -0,0 +1,35 @@
+package com.example.market.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketStompConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chatting");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration){
+        System.out.println("registration = " + registration);
+        System.out.println("여기서 헤더 검증해?");
+        registration.interceptors(stompHandler);
+    }
+}
